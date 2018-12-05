52a14e8251ac1238a43ba4c223bc9fa574e18d0e
akkarin <at.mattenn@gmail.com>
2018-12-05 13:31:33 +0900

    Initial Commit

---
+++ saicoro/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  saicoro
+//
+//  Created by 友利 奈緒 on 2018/12/05.
+//  Copyright © 2018年 MATTENN. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
